06d2f62061f39ef921e0f7f1421fa6d256e6ba97
MeAhtisham <ashtishamrana8@gmail.com>
2021-07-17 07:11:19 +0500

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderSection></HeaderSection>
+    <Aboutme></Aboutme>
+    <Counter></Counter>
+    <Gallery />
+    <Education></Education>
+    <Services></Services>
+    <Sponsorssec></Sponsorssec>
+    <Formsec></Formsec>
+    <Map></Map>
+    <Footer></Footer>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HeaderSection from "./container/HeaderSection";
+import Aboutme from "./container/Aboutme.vue";
+import Counter from "./container/CounterSec.vue";
+import Gallery from "./container/Gallery.vue";
+import Education from "./container/Education.vue";
+import Services from "./container/Services.vue";
+import Sponsorssec from "./container/Sponsorssec.vue";
+import Formsec from "./container/Formsec.vue";
+import Map from "./container/Map.vue";
+import Footer from "./container/Footer.vue";
+// import HelloWorld from "./components/HelloWorld";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HeaderSection,
+    Aboutme,
+    Counter,
+    Gallery,
+    Education,
+    Services,
+    Sponsorssec,
+    Formsec,
+    Map,
+    Footer,
+    // Card,
+    // Hell
+    // oWorld,
+  },
+  data() {
+    return {
+      hed: "i am heading you just creatred using props",
+      para: "i am paara",
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
